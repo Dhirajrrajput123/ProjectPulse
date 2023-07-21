fda9423e49a318db4bc747698ca0ebe9ce62643e
Goldy Rajput <dhirajrajpp@gmail.com>
2023-07-21 06:37:45 +0530

    m3 completed and backend competed

--- projectPulse/main.py
+++ projectPulse/main.py
@@ -3,6 +3,7 @@ from bson import ObjectId
 from pymongo import MongoClient
 import json
 from datetime import datetime
+from flask_cors import CORS
 
 class JSONEncoder(json.JSONEncoder):
     def default(self, o):
@@ -11,6 +12,7 @@ class JSONEncoder(json.JSONEncoder):
         return super().default(o)
 
 app = Flask(__name__)
+CORS(app)  # Initialize Flask-Cors with your app
 app.json_encoder = JSONEncoder
 
 connection_url='mongodb+srv://dhirajrajpp:root@cluster0.slgcdts.mongodb.net/?retryWrites=true&w=majority'
@@ -68,7 +70,7 @@ def update_managers(mId):
 def delete_managers(mId):
     length=db.manager.delete_one({'managerId':mId})
     if length.deleted_count>0:
-        return jsonify({'message':'Manager deleted... '})
+        return jsonify({'message':'Manager deleted... '}),200
     else:
         return jsonify({'message': 'Id not matches... '}),404
 
@@ -100,6 +102,297 @@ def get_managers():
 
 
 # Manager end Point end here
+#===========================================================================================================
+# Project end Point Start here
 
+# Add project
+@app.route("/project",methods=['POST'])
+def addProject():
+    data=request.get_json()
+    id=data['managerId']
+    print(id)
+    managers=db.manager.find_one({'managerId':id})
+    if managers==None:
+        return jsonify({'message':'Manager Id not present '})
+
+    projects = list(db.project.find())
+    length = len(projects)
+    if length == 0:
+        data['projectId'] = 1
+    else:
+        data['projectId'] = projects[length - 1]['projectId'] + 1
+
+    db.project.insert_one(data)
+
+    return jsonify({'message':'Project added successfully'}),200
+
+
+
+
+# Update project
+@app.route('/project/<int:pId>',methods=['PUT'])
+def update_projects(pId):
+    data=request.get_json()
+    update_query = {'$set': data}
+    result=db.project.update_one({'projectId': pId}, update_query)
+
+    if result.modified_count>0:
+        return jsonify({'message': 'project update... '}), 200
+    else:
+        return jsonify({'message': 'project Id not matches... '}), 404
+
+
+
+
+#     Delete project
+@app.route('/project/<int:pId>',methods=['DELETE'])
+def delete_projects(pId):
+    length=db.project.delete_one({'projectId':pId})
+    if length.deleted_count>0:
+        return jsonify({'message':'project deleted... '}),200
+    else:
+        return jsonify({'message': 'Id not matches... '}),404
+
+
+
+
+# Get project
+@app.route('/project/<int:pId>',methods=['GET'])
+def get_project(pId):
+    project=db.project.find_one({'projectId':pId})
+    if project:
+        project_json = json.dumps(project, cls=JSONEncoder)
+        return project_json, 200
+    else:
+        return jsonify({'message':'project not present in this id'}),404
+
+
+
+# Get project
+@app.route('/project/manager/<int:mId>',methods=['GET'])
+def get_projectmanager(mId):
+    projects = db.project.find({'managerId':mId})
+    print(projects)
+    if projects:
+        documents = [document for document in projects]
+        project_json = json.dumps(documents, default=JSONEncoder().default)
+        return project_json, 200
+
+    return jsonify({'message': 'project not present'}), 404
+
+# get all project
+@app.route('/project',methods=['GET'])
+def get_projects():
+    projects=db.project.find()
+    print(projects)
+    if projects:
+        documents = [document for document in projects]
+        project_json = json.dumps(documents, default=JSONEncoder().default)
+        return project_json, 200
+
+    return jsonify({'message':'project not present'}),404
+
+
+# project end point end here
+# =================================================================================================================================
+# Task end Point Start Here
+
+@app.route("/task",methods=['POST'])
+def addtask():
+    data=request.get_json()
+    id=data['projectId']
+    rId=data['resourceId']
+    print(id)
+    projects=db.project.find_one({'projectId':id})
+    if projects==None:
+        return jsonify({'message':'project Id not present '})
+
+    resources = db.resource.find_one({'resourceId': rId})
+    if resources == None:
+        return jsonify({'message': 'resource Id not present '})
+
+    tasks = list(db.task.find())
+    length = len(tasks)
+    if length == 0:
+        data['taskId'] = 1
+    else:
+        data['taskId'] = tasks[length - 1]['taskId'] + 1
+
+    db.task.insert_one(data)
+
+    return jsonify({'message':'Task added successfully'}),200
+
+
+
+
+# Update project
+@app.route('/task/<int:tId>',methods=['PUT'])
+def update_task(tId):
+    data=request.get_json()
+    update_query = {'$set': data}
+    result=db.task.update_one({'taskId': tId}, update_query)
+
+    if result.modified_count>0:
+        return jsonify({'message': 'Task update... '}), 200
+    else:
+        return jsonify({'message': 'Task Id not matches... '}), 404
+
+
+
+
+#     Delete project
+@app.route('/task/<int:tId>',methods=['DELETE'])
+def delete_task(tId):
+    length=db.task.delete_one({'taskId':tId})
+    if length.deleted_count>0:
+        return jsonify({'message':'task deleted... '}),200
+    else:
+        return jsonify({'message': 'Id not matches... '}),404
+
+
+
+
+# Get project
+@app.route('/task/<int:tId>',methods=['GET'])
+def get_task(tId):
+    task=db.task.find_one({'taskId':tId})
+    if task:
+        task_json = json.dumps(task, cls=JSONEncoder)
+        return task_json, 200
+    else:
+        return jsonify({'message':'task not present in this id'}),404
+
+
+
+# Get projectId
+@app.route('/task/project/<int:pId>',methods=['GET'])
+def get_task_project(pId):
+    tasks = db.task.find({'projectId':pId})
+    print(tasks)
+    if tasks:
+        documents = [document for document in tasks]
+        project_json = json.dumps(documents, default=JSONEncoder().default)
+        return project_json, 200
+
+    return jsonify({'message': 'tasks not present'}), 404
+
+# get task by developerId
+@app.route('/task/developer/<int:dId>',methods=['GET'])
+def get_task_developer(dId):
+    tasks = db.task.find({'developerId':dId})
+    print(tasks)
+    if tasks:
+        documents = [document for document in tasks]
+        project_json = json.dumps(documents, default=JSONEncoder().default)
+        return project_json, 200
+
+    return jsonify({'message': 'tasks not present'}), 404
+
+
+# get task by resourceId
+@app.route('/task/resource/<int:rId>',methods=['GET'])
+def get_task_resource(rId):
+    tasks = db.task.find({'resourceId':rId})
+    print(tasks)
+    if tasks:
+        documents = [document for document in tasks]
+        project_json = json.dumps(documents, default=JSONEncoder().default)
+        return project_json, 200
+
+    return jsonify({'message': 'tasks not present'}), 404
+
+
+
+# get all Task
+@app.route('/task',methods=['GET'])
+def get_All_task():
+    tasks=db.task.find()
+    print(tasks)
+    if tasks:
+        documents = [document for document in tasks]
+        project_json = json.dumps(documents, default=JSONEncoder().default)
+        return project_json, 200
+
+    return jsonify({'message':'tasks not present'}),404
+
+# task end point end here
+# ===============================================================================================================================
+
+# resource end Point start here
+
+# add resource
+@app.route('/resource',methods=['POST'])
+def add_resource():
+    data=request.get_json()
+
+    resources=list(db.resource.find())
+    length=len(resources)
+    if length==0:
+        data['resourceId']=1
+    else:
+        data['resourceId']=resources[length-1]['resourceId']+1
+
+    db.resource.insert_one(data)
+
+    return jsonify({'message':"resource added Successfully"})
+
+
+
+
+# Update Resource
+@app.route('/resource/<int:rId>',methods=['PUT'])
+def update_resource(rId):
+    data=request.get_json()
+    update_query = {'$set': data}
+    result=db.resource.update_one({'resourceId': rId}, update_query)
+
+    if result.modified_count>0:
+        return jsonify({'message': 'resource update... '}), 200
+    else:
+        return jsonify({'message': 'resource Id not matches... '}), 404
+
+
+
+
+#     Delete Resource
+@app.route('/resource/<int:rId>',methods=['DELETE'])
+def delete_resource(rId):
+    length=db.resource.delete_one({'resourceId':rId})
+    if length.deleted_count>0:
+        return jsonify({'message':'Resource deleted... '}),200
+    else:
+        return jsonify({'message': 'resource Id not matches...  '}),404
+
+
+
+
+# Get Resource
+@app.route('/resource/<int:rId>',methods=['GET'])
+def get_resource(rId):
+    resources=db.resource.find_one({'resourceId':rId})
+    if resources:
+        manager_json = json.dumps(resources, cls=JSONEncoder)
+        return manager_json, 200
+    else:
+        return jsonify({'message':'resource not present in this id'}),404
+
+
+# get all Resource
+@app.route('/resource',methods=['GET'])
+def get_resources():
+    resources=db.resource.find()
+    print(resources)
+    if resources:
+        documents = [document for document in resources]
+        manager_json = json.dumps(documents, default=JSONEncoder().default)
+        return manager_json, 200
+        #  return jsonify(managers)
+    return jsonify({'message':'resource not present'}),404
+
+
+
+# resource end point end here
+
+# =====================================================================================================================
 if __name__ == '__main__':
        app.run(debug=True)
---
+++ projectPulse_frontEnd/project_pulse/src/components/manager/managerList.js
@@ -0,0 +1,92 @@
+import React from 'react'
+import { useState, useEffect, } from 'react'
+import UpdateManager from './UpdateManager'
+import CreateManager from './createManager'
+import { Link, useSearchParams } from 'react-router-dom'
+
+
+
+const ManagerList = () => {
+    // const [serachParam,setSearchParam]=useSearchParams();
+    const [manager, setManager] = useState([]);
+    // const [ele, setEle] = useState({});
+    
+    // const [name, setName] = useState("");
+    // const [role, setRole] = useState("");
+    // const [bio, setBio] = useState("");
+    // const [status, setStatus] = useState("");
+    // const [date, setdate] = useState("");
+    
+    const [flag,setFlag]=useState(false);
+    const [dis,setDis]=useState(false);
+
+    const fetchData = async () => {
+        try {
+            const response = await fetch('http://127.0.0.1:5000/manager');
+            const data = await response.json();
+            setManager(data);
+        } catch (error) {
+            alert('Error fetching data: ', error);
+        }
+
+    }
+    useEffect(() => {
+
+        console.log("inside the effect");
+        fetchData();
+    }, [flag]);
+
+    const deleteManager = async (id) => {
+        console.log(id);
+        try {
+            const response = await fetch("http://127.0.0.1:5000/manager/" + id, {
+                method: "DELETE"
+            });
+            const data = await response.json();
+            console.log(data);
+            fetchData();
+        } catch (error) {
+            alert('Error fetching data: ', error);
+        }
+    }
+
+    const updateManager = (element) => {
+        setDis(!dis)
+        localStorage.setItem("manager",JSON.stringify(element));
+    }
+
+    return (
+        <div>
+            <div>
+                {
+                    manager.map((element, index) => {
+                        return (
+
+                            <div key={element.managerId} className='manager_child'>
+                                <div>{element.managerId}</div>
+                                <div>{element.name}</div>
+                                <div>{element.role}</div>
+                                <div>{element.bio}</div>
+                                <div>{element.start_data}</div>
+                                <div>{element.status ? "Active" : "InActive"}</div>
+                                <div><button onClick={()=>updateManager(element)}>Update</button></div>
+                                <div> <button onClick={() => deleteManager(element.managerId)}>Delete</button></div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+            <div>
+                <CreateManager fetchData={fetchData} />
+
+            </div>
+
+            <div className='update_manager' style={{display:dis?"inline":"none"}}>
+               <UpdateManager/>
+            </div>
+
+        </div>
+    )
+}
+
+export default ManagerList
